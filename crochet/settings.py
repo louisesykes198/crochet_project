d4b81adf23a476901db98d511bb88cf358ff3bb4
louisesykes198 <louisesykes198@yahoo.com>
2025-04-10 12:29:12 +0100

    Added crochet_store to INSTALLED_APPS

--- crochet/settings.py
+++ crochet/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crochet_store',
 ]
 
 MIDDLEWARE = [
--- crochet_store/models.py
+++ crochet_store/models.py
@@ -6,6 +6,9 @@ class CrochetItem(models.Model):
     image = models.ImageField(upload_to='crochet_images/')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     
+    class Meta:
+        app_label = 'crochet_store'
+    
     def __str__(self):
         return self.name
 
