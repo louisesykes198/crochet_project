a66bae889cfdff059a8e663ee7adcf466d1926f9
louisesykes198 <louisesykes198@yahoo.com>
2025-04-10 13:06:51 +0100

    Changed location of html files

--- crochet_store/views.py
+++ crochet_store/views.py
@@ -4,7 +4,7 @@ from .forms import CrochetItemForm
 
 def index(request):
     items = CrochetItem.objects.all()
-    return render(request, 'crochet_store/index.html', {'items': items})
+    return render(request, 'index.html', {'items': items})
 
 def create_item(request):
     if request.method == 'POST':
@@ -14,7 +14,7 @@ def create_item(request):
             return redirect('index')
     else:
         form = CrochetItemForm()
-    return render(request, 'crochet_store/form.html', {'form': form})
+    return render(request, 'form.html', {'form': form})
 
 def edit_item(request, id):
     item = get_object_or_404(CrochetItem, id=id)
@@ -25,11 +25,11 @@ def edit_item(request, id):
             return redirect('index')
     else:
         form = CrochetItemForm(instance=item)
-    return render(request, 'crochet_store/form.html', {'form': form})
+    return render(request, 'form.html', {'form': form})
 
 def delete_item(request, id):
     item = get_object_or_404(CrochetItem, id=id)
     if request.method == 'POST':
         item.delete()
         return redirect('index')
-    return render(request, 'crochet_store/delete_confirm.html', {'item': item})
+    return render(request, 'delete_confirm.html', {'item': item})
